e97b13f9f18532e2ad50e9c18930881d858a66df
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-24 18:25:44 +0530

    chore: add authorization to access account page

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Your Account",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7">Hello Amit,</h2>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Hello {session.user.name.split(" ").at(0)},
+    </h2>
   );
 }
