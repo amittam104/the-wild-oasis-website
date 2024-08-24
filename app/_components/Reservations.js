84a6c3285fd483c74cfc5e9bf6c9baff2abb2b1c
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-24 08:32:37 +0530

    chore: display loged in user

--- app/_components/Reservations.js
+++ app/_components/Reservations.js
@@ -1,10 +1,14 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 async function Reservations({ cabin }) {
   const settings = await getSettings();
   const bookedDates = await getBookedDatesByCabinId(cabin.id);
+
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
@@ -13,7 +17,11 @@ async function Reservations({ cabin }) {
         cabin={cabin}
         bookedDates={bookedDates}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session?.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
